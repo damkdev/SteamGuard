93e9cebfa88d4435c8c729fe66bffc55cf886ea1
damkdev <damian.korz@gmail.com>
2023-10-27 01:11:45 -0500

    implement background worker interval from app settings

--- Service.cs
+++ Service.cs
@@ -2,19 +2,21 @@ namespace SteamGuard.Worker
 {
     public class Service : BackgroundService
     {
+        private readonly IConfiguration _configuation;
         private readonly ILogger<Service> _logger;
 
-        public Service(ILogger<Service> logger)
+        public Service(IConfiguration configuration, ILogger<Service> logger)
         {
+            _configuation = configuration;
             _logger = logger;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
+        {            
             while (!stoppingToken.IsCancellationRequested)
-            {
+            {                
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(TimeSpan.FromMinutes(_configuation.GetValue<int>("IntervalMinutes")), stoppingToken);
             }
         }
     }
