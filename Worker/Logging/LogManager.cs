d97110f1bf31ea83b404fdda710294c85dd1ca5b
damkdev <damian.korz@gmail.com>
2023-10-30 23:57:30 -0500

    add nlog dependency; implement and configure nlog; add archiving method

---
+++ Worker/Logging/LogManager.cs
@@ -0,0 +1,21 @@
+﻿namespace SteamGuard.Worker.Logging
+{
+    public static class LogManager
+    {
+        public static void Archive()
+        {            
+            var logDir = Path.Combine(new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory).Parent?.FullName ?? string.Empty, "Log");
+            if (!string.IsNullOrEmpty(logDir))
+            {
+                if (!Directory.Exists(logDir))
+                    Directory.CreateDirectory(logDir);
+                var sourceDir = new DirectoryInfo(logDir);
+                var targetDir = sourceDir.CreateSubdirectory($"Archive_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}");
+                foreach (var log in sourceDir.GetFileSystemInfos("*.log", SearchOption.TopDirectoryOnly))
+                {
+                    File.Move(log.FullName, Path.Combine(targetDir.FullName, log.Name));
+                }
+            }
+        }
+    }
+}
--- Worker/Program.cs
+++ Worker/Program.cs
@@ -1,8 +1,30 @@
+using NLog;
+using NLog.Web;
+
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
-    {        
+    {
         services.AddHostedService<SteamGuard.Worker.Service>();
     })
+    .ConfigureLogging((hostContext, logging) =>
+    {
+        SteamGuard.Worker.Logging.LogManager.Archive();
+        logging.ClearProviders();
+        logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+    })
+    .UseNLog()
     .Build();
 
-host.Run();
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+try
+{
+    host.Run();
+}
+catch (Exception ex)
+{
+    logger.Error(ex);
+}
+finally
+{
+    LogManager.Shutdown();
+}
